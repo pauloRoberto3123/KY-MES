d2c27c9a7401e715c706c1fb8eeb7ea3f5e0a200
Paulo Neto <paulo_roberto127@hotmail.com>
2025-06-10 16:23:22 -0400

    No WipId on body

--- KY-MES.Domain/V1/DTOs/OutputModels/CompleteWipIoTRequestModel.cs
+++ KY-MES.Domain/V1/DTOs/OutputModels/CompleteWipIoTRequestModel.cs
@@ -9,7 +9,7 @@ namespace KY_MES.Domain.V1.DTOs.OutputModels
 {
     public class CompleteWipIoTRequestModel
     {
-        public int WipId { get; set; }
+        //public int WipId { get; set; }
         public DateTime EndDateTimeString { get; set; }
         public bool IsSingleWIPMode { get; set; }
         public string? ValidateSetupSheet { get; set; }
--- KY-MES.Services/MESService.cs
+++ KY-MES.Services/MESService.cs
@@ -170,7 +170,6 @@ namespace KY_MES.Services
 
                 var completeWipIoTRequestModel = new CompleteWipIoTRequestModel
                 {
-                    WipId = wipId,
                     IsSingleWIPMode = true
                 };
 
